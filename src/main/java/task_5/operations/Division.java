90acef896dc1379c3f4f01a442a82b4d0cdcfebd
Aleksey <chetve007@mail.ru>
2019-03-08 15:55:55 +0300

    task #7 (exceptions, add to task_5/operations/Division)

--- src/main/java/task_5/operations/Division.java
+++ src/main/java/task_5/operations/Division.java
@@ -3,8 +3,19 @@ package task_5.operations;
 public class Division {
 
     public double operation(double a, double b) {
-        if (b == 0.0)
-            throw new ArithmeticException("You cannot divide by 0");
-        return a / b;
+//        if (b == 0.0)                                                     first version
+//            throw new ArithmeticException("You cannot divide by 0");
+//        return a / b;
+
+
+        double res = 0;
+        try {
+            res = a / b;
+            if (res == Double.POSITIVE_INFINITY || res == Double.NEGATIVE_INFINITY)
+                throw new ArithmeticException();
+        } catch (ArithmeticException e) {
+            System.out.println("What are you doing? You can't divide by 0!");
+        }
+        return res;
     }
 }
